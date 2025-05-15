541b4f35e87e0f7d21ef79fc63c62ded2cec1ad4
Silmiuww <nadiasalma078@gmail.com>
2025-05-15 19:38:09 +0700

    update kode modif no3

--- Pertemuan12/SLLMain12.java
+++ Pertemuan12/SLLMain12.java
@@ -1,4 +1,7 @@
 package Pertemuan12;
+
+import java.util.Scanner;
+
 public class SLLMain12 {
     public static void main(String[] args) {
         SingleLinkedList12 sll = new SingleLinkedList12();
@@ -8,13 +11,17 @@ public class SLLMain12 {
         Mahasiswa12 mhs3 = new Mahasiswa12("22212202", "Cintia", "3C", 3.5);
         Mahasiswa12 mhs4 = new Mahasiswa12("23212201", "Bimon", "2B", 3.8);
 
-        sll.print();
-        sll.addFirst(mhs1);
-        sll.print();
-        sll.addLast(mhs2);
-        sll.print();
-        sll.insertAt(1, mhs3);
-        sll.insertAfter("21212203", mhs4);
-        sll.print();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan NIM: ");
+        String nim = sc.nextLine();
+        System.out.print("Masukkan Nama: ");
+        String nama = sc.nextLine();
+        System.out.print("Masukkan Kelas: ");
+        String kelas = sc.nextLine();
+        System.out.print("Masukkan IPK: ");
+        double ipk = sc.nextDouble();
+        Mahasiswa12 mhs = new Mahasiswa12(nim, nama, kelas, ipk);
+        sll.addFirst(mhs);
+
     }
 }
